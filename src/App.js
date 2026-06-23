c8718af3aca19985283f19eb224b29bf1be6c4b7
Gift Amachree <amakrigift2000@gmail.com>
2026-06-23 11:24:22 +0100

    styling my pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
